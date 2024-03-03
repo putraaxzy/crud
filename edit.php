7251c32d98e0bfeecc43ce7f65fab8ab072d4a32
ptra <144090584+putraaxzy@users.noreply.github.com>
2024-03-03 18:15:53 +0700

    initial commit
    bajigur

---
+++ edit.php
@@ -0,0 +1,22 @@
+<?php
+include "db_conn.php";
+$id = $_GET["id"];
+
+if (isset($_POST["submit"])) {
+  $first_name = $_POST['first_name'];
+  $last_name = $_POST['last_name'];
+  $email = $_POST['email'];
+  $gender = $_POST['gender'];
+
+  $sql = "UPDATE `crud` SET `first_name`='$first_name',`last_name`='$last_name',`email`='$email',`gender`='$gender' WHERE id = $id";
+
+  $result = mysqli_query($conn, $sql);
+
+  if ($result) {
+    header("Location: index.php?msg=Data updated successfully");
+  } else {
+    echo "Failed: " . mysqli_error($conn);
+  }
+}
+
+?>
